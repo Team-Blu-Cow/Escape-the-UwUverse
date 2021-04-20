b736ce0b1360605006bf5f853e48bc2ac0bffcc6
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2021-04-20 16:03:39 +0100

    camera snaps to closest anchor

---
+++ Escape the UwUverse/Assets/Resources/Scripts/Grid/RoomController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    private List<GameObject> rooms = new List<GameObject>();
+
+    private GameObject m_closest;
+    private GameObject m_player;
+
+    private void Start()
+    {
+        m_player = GameObject.Find("Player");
+    }
+
+    public void AddRoom(GameObject roomObject)
+    {
+        if (!rooms.Contains(roomObject))
+        {
+            rooms.Add(roomObject);
+        }
+    }
+
+    public void RemoveRoom(GameObject roomObject)
+    {
+        rooms.Remove(roomObject);
+    }
+
+    private void FixedUpdate()
+    {
+        float shortest = 0f;
+        GameObject closest = null;
+
+        foreach (GameObject obj in rooms)
+        {
+            float dist = Vector3.Distance(obj.transform.position, m_player.transform.position);
+            if (closest == null)
+            {
+                closest = obj;
+                shortest = dist;
+                continue;
+            }
+
+            if (dist < shortest)
+            {
+                closest = obj;
+                shortest = dist;
+            }
+        }
+
+        if (closest != m_closest)
+        {
+            if (m_closest != null)
+                m_closest.GetComponent<Room>().m_focused = false;
+
+            if (closest != null)
+                closest.GetComponent<Room>().m_focused = true;
+
+            m_closest = closest;
+        }
+    }
+
+    // TODO @matthew - conver this over to lean twean
+    private void Update()
+    {
+        Vector3 cameraPos = Camera.main.transform.position;
+        cameraPos = Vector3.Lerp(cameraPos, m_closest.transform.position, 0.05f);
+        Camera.main.transform.position = cameraPos;
+    }
+}
